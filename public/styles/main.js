65a22aed3310438ac872b5bdac9d8d27a59092c4
jhamadhav <jhamadhav28@gmail.com>
2023-05-04 14:58:40 +0530

    two input added for filter images

--- public/styles/main.js
+++ public/styles/main.js
@@ -53,6 +53,39 @@ const makeFilterDivContent = () => {
         // console.log(id);
         // console.log(imageCount);
 
+        if (imageCount == "2") {
+            divs[i].innerHTML = `
+            <h2 class="text-center">Upload image for demo</h2>
+
+            <!-- change ID as per your filter -->
+            <input type="file" name="imageFile" id="${id}-filter-inp" class="file-inputs" autocomplete="off">
+
+            <input type="file" name="imageFile" id="${id}-filter-inp-2" class="file-inputs" autocomplete="off">
+
+            <br><br>
+
+            <!-- change the image ID as per your filter -->
+            <span>Original Image:</span>
+            <br>
+
+
+            <img src="" id="${id}-filter-img-inp" alt="image-1-input-here">
+            <br>
+            <img src="" id="${id}-filter-img-inp-2" alt="image-2-input-here">
+
+            <br><br>
+            <span>Filter Image:</span>
+
+            <br>
+            <img src="" id="${id}-filter-img-out" alt="image-output-here">
+
+            <!-- change the btn id as per your filter name -->
+            <button id="${id}-filter-btn"
+                onclick='getFilter("${id}","${id}-filter-inp","${id}-filter-img-inp","${id}-filter-img-out","${id}-filter-inp-2","${id}-filter-img-inp-2" )'>Send</button>
+            </p>`
+            continue
+        }
+
         divs[i].innerHTML = `
             <h2 class="text-center">Upload image for demo</h2>
 
@@ -84,7 +117,7 @@ const makeFilterDivContent = () => {
 
 async function getFilter(filterName, inp1, imgInp1, imgOut1, inp2 = null, imgInp2 = null) {
     if (inp1 == null || filterName == null) return;
-    console.log("hello");
+
 
     let img1Base64 = imageFilesMap[inp1]
 
@@ -96,7 +129,7 @@ async function getFilter(filterName, inp1, imgInp1, imgOut1, inp2 = null, imgInp
 
     let img2Base64 = imageFilesMap[inp2]
     let img2 = null
-    if (imgInp2 != null) {
+    if (inp2 != null) {
         document.getElementById(imgInp2).src = img2Base64
         img2 = img2Base64.split("base64,")[1]
     }
@@ -104,7 +137,8 @@ async function getFilter(filterName, inp1, imgInp1, imgOut1, inp2 = null, imgInp
     let data = {
         "imageFile1": img1,
         "imageFile2": img2,
-        "filter": filterName
+        "filter": filterName,
+        "imageCount": ((inp2 == null) ? 1 : 2)
     }
     // console.log(data);
 
